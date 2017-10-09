0334f091842d68dd5dedf5d0214d4da6e5485eb4
Felipe Gonzalez <felipexgonzalez@gmail.com>
2017-10-09 00:13:59 -0500

    Usar versión fija de glmnet

--- _build.sh
+++ _build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
+Rscript -e "devtools::install_version('glmnet', version = '2.0-10', repos = 'http://cran.us.r-project.org')"
 Rscript -e "keras::install_keras()"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-
